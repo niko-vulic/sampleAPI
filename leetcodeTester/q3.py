d179672b00c3d449282daa2bbffcda01698a7e1f
Niko Vulic <niko.vulic@outlook.com>
2021-08-25 13:06:30 -0400

    1.1.1

--- leetcodeTester/q3.py
+++ leetcodeTester/q3.py
@@ -1,3 +1,33 @@
 # 3. Longest Substring Without Repeating Characters
 # Given a string s, find the length of the longest substring without repeating characters.
 
+
+
+class Solution:
+    def __init__(self):
+        self.testCases = []
+        self.funcName = "lengthOfLongestSubstring"
+
+    def defineTestCases(self):
+        self.testCases.append("abcabc")
+        self.testCases.append("bbbeeee")
+
+    def runTests(self):
+        for case in self.testCases:
+            print("Test case:" + case + ", answer is:" + str(getattr(self, self.funcName)(case)))
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        totalLength = len(s)
+        current  = []
+
+        for i in s:
+            pass
+
+        maxLength = 0
+
+        return maxLength
+
+if __name__ == '__main__':
+    soln = Solution()
+    soln.defineTestCases()
+    soln.runTests()
