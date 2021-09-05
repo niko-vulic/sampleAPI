ff83ba34c26b83abf4a5596437ebcc4ccdbd56e1
Niko Vulic <niko.vulic@outlook.com>
2021-09-05 17:45:34 -0400

    1.8

---
+++ leetcodeTester/q49.py
@@ -0,0 +1,70 @@
+# 49. Group Anagrams
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+
+from typing import List
+
+class Solution:
+    def __init__(self):
+        self.testCases = []
+        self.funcName = "groupAnagrams"
+
+    def defineTestCases(self):
+        self.testCases.append(["eat","tea","tan","ate","nat","bat"])
+        #self.testCases.append(["a"])
+        self.testCases.append(["","b",""])
+        #self.testCases
+
+    def runTests(self):
+        for case in self.testCases:
+            print("Test case:" + str(case) + ", answer is:" + str(getattr(self, self.funcName)(case)))
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        resultDict = {}
+
+        for item in strs:
+            key = ''.join(sorted(item))
+            if key in resultDict:
+                resultDict[key].append(item)
+            else:
+                resultDict[key] = [item]
+
+        return list(resultDict.values())
+
+    def groupAnagrams_working_for_short_arrays(self, strs: List[str]) -> List[List[str]]:
+        resultSet = []
+
+        while strs:
+            candidate = strs.pop()
+            subset = []
+            isValidMatch = [True] * len(strs)
+
+            for other in range(len(strs)):
+                if len(candidate) != len(strs[other]):
+                    isValidMatch[other] = False
+                else:
+                    for str_char in candidate:
+                        if isValidMatch[other] != False:
+                        #print("Comparing -" + candidate + "- and -" + strs[other])
+                            if candidate.count(str_char) != strs[other].count(str_char):
+                                isValidMatch[other] = False
+
+            for match in range(len(strs)):
+                if isValidMatch[match]:
+                    subset.append(strs[match])
+
+            print("Candidate is: " + candidate + " ,matches found:" + str(subset))
+            for item in subset:
+                print("Attempting to remove:" + item)
+                strs.remove(item)
+
+            # Append the candidate before returning, else it will fail on remove as we already popped it
+            subset.append(candidate)
+            resultSet.append(subset)
+        return resultSet
+
+if __name__ == '__main__':
+    soln = Solution()
+    soln.defineTestCases()
+    soln.runTests()
---
+++ leetcodeTester/skeletons/stringInput.py
@@ -0,0 +1,48 @@
+# 3. Longest Substring Without Repeating Characters
+# Given a string s, find the length of the longest substring without repeating characters.
+
+
+
+class Solution:
+    def __init__(self):
+        self.testCases = []
+        self.funcName = "funcName"
+
+    def defineTestCases(self):
+        self.testCases.append("stringHere")
+        self.testCases.append("bbbeeee")
+        self.testCases.append("12341234512345612345")
+
+    def runTests(self):
+        for case in self.testCases:
+            print("Test case:" + case + ", answer is:" + str(getattr(self, self.funcName)(case)))
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Moving sub-array problem. We use a second array to store the current, and a maxLength counter which is
+        # recalculated when the current is increased. No further variables are required
+        maxLength = 0
+
+        # Its possible a dict would be faster runtime than an array
+        currentSubstring  = []
+
+
+        for i in s:
+            if i not in currentSubstring:
+                # If the element doesn't already exist, add it and recalc max length
+                currentSubstring.append(i)
+
+                # Everytime we increase the currentSubstring, it might be possible to become a new max
+                if len(currentSubstring) > maxLength:
+                    maxLength = len(currentSubstring)
+            else:
+                # If the element exists, slice the current array from the element and append the same char to the end
+                indexOfElement = currentSubstring.index(i)
+                currentSubstring = currentSubstring[indexOfElement+1:]
+                currentSubstring.append(i)
+
+        return maxLength
+
+if __name__ == '__main__':
+    soln = Solution()
+    soln.defineTestCases()
+    soln.runTests()
