395b89499f2ad24fca4f0487d28e4c7c4dcaed12
Niko Vulic <niko.vulic@outlook.com>
2021-09-16 23:35:16 -0400

    1.14

--- leetcodeTester/q55.py
+++ leetcodeTester/q55.py
@@ -15,7 +15,24 @@ class Solution:
         for case in self.testCases:
             print("Test case:" + str(case) + ", answer is:" + str(getattr(self, self.funcName)(case)))
 
+    def canJumpLambda(self, nums: List[int]) -> bool:
+
+        return reduce(lambda x: )
+
+        if len(nums) == 1: return True
+        currentJumpCounter = nums[0]
+
+        for i in range(len(nums)):
+            currentJumpCounter = currentJumpCounter - 1
+            if currentJumpCounter >= 0:
+                currentJumpCounter = max(currentJumpCounter, nums[i])
+            else:
+                return False
+
+        return True
+
     def canJump(self, nums: List[int]) -> bool:
+
 
         if len(nums) == 1: return True
         currentJumpCounter = nums[0]
